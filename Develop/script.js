5666a7b6b9f8dfb99f3221656c8d99a04af2f558
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-05-19 01:42:08 -0700

    updates

--- Develop/script.js
+++ Develop/script.js
@@ -5,32 +5,37 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+// listed all posible pasword inputs
 
 function generatePassword(){
-  let capLetters "ABCDEFGHIJKLMNPQRSTYVWXYZ";
-  let lowLetters "abcdefghijklmnopqrstuvwxyz";
-  let specials "!@#$%^&*()_+-=><";
-  let numbers "1234567890"
+  let capLetters = "ABCDEFGHIJKLMNPQRSTYVWXYZ";
+  let lowLetters = "abcdefghijklmnopqrstuvwxyz";
+  let specials = "!@#$%^&*()_+-=><";
+  let numbers = "1234567890"
+
+//Used split method to split all the strings into substrings
+//used empty string as the separator to split bewteen each character
 
   let cap = capLetters.split("");
   let low = lowLetters.split("");
   let spe = specials.split("")
   let num = numbers.split("")
+
   let characters = [];
   let password = "";
-  let pwchars = prompt("how many charaters do you want? min 8, max 128")
+  let pwchars = prompt("how many charaters do you want? min 8, max 128");
   if (pwchars < 8 | pwchars > 128){
-    alert("password must be at least 8 charters and less than 128 charters! refresh your browser and try again!")
+    alert("password must be at least 8 charters and less than 128 charters! refresh your browser and try again!");
     return
   };
-// Not a Number
+// Not a Number so if they type something thats not a number it will give an alert
   if(isNaN(pwchars) === true){
     alert("please use a valid number! refresh your browser and try again!")
     return
@@ -40,34 +45,29 @@ function generatePassword(){
   let capital = confirm("Do you want your password to have upper case letters?");
   let numbs = confirm("Do you want your password to have numbers?");
   let specs = confirm("Do you want your password to have special characters?");
-
-  if(lower === true){
+// statements in order to see what characters to add to random password
+  if(lower == true){
     characters = characters.concat(low);
   }
-  if(capital === true){
+  if(capital == true){
     characters = characters.concat(cap);
   }
-  if(numbs === true){
+  if(numbs == true){
     characters = characters.concat(num);
   }
-  if(specs === true){
+  if(specs == true){
     characters = characters.concat(spe);
   }
-  if(characters.length === 0);{
-    alert("must choose character types")
+  if(characters.length == 0) {
+    alert("must choose character types");
   }
-  
-
-
-
-
-
-
-
-
-
-
-
+    else {
+      for (let i = 0;i < pwchars; i++){
+        let generatePassword = Math.floor(Math.random() * characters.length);
+        password = password + characters[generatePassword];
+      }
+    }
+  return password;
 }
 
 
